692036edc5f3f0e378eb24468ab0a8f55c4e68c1
Ryan Cantone <ryan.cantone@fullsail.edu>
2013-07-08 21:58:05 -0400

    First commit VFW P4
    This is adding folder to include VFW Project 4 Files for week 1 of MIU

---
+++ VFW-Project-4/JSON.js
@@ -0,0 +1,98 @@
+var json = {
+    "weapon1": {
+        "weaponName": ["Weapon Name:", "Beretta Nano"],
+        "weaponCategory": ["Weapon Category:", "Handgun"],
+        "deadly": ["Deadly:", "Yes"],
+        "favorite": ["Favorite:", "Yes"],
+        "difficulty": ["Difficulty:", "4"],
+        "otherDetails": ["Other Details:", "An ultra-compact 9mm pistol."]
+    },
+    "weapon2": {
+        "weaponName": ["Weapon Name:", "Uzi Carbine"],
+        "weaponCategory": ["Weapon Category:", "SMG"],
+        "deadly": ["Deadly:", "Yes"],
+        "favorite": ["Favorite:", "Yes"],
+        "difficulty": ["Difficulty:", "6"],
+        "otherDetails": ["Other Details:", "A legal civilian variant of the popular Uzi."]
+    },
+    "weapon3": {
+        "weaponName": ["Weapon Name:", "Serbu Super Shorty 9"],
+        "weaponCategory": ["Weapon Category:", "Shotgun"],
+        "deadly": ["Deadly:", "Yes"],
+        "favorite": ["Favorite:", "No"],
+        "difficulty": ["Difficulty:", "8"],
+        "otherDetails": ["Other Details:", "A very compact 12 gauge shotgun with a pistol grip."]
+    },
+    "weapon4": {
+        "weaponName": ["Weapon Name:", "Barrett 82A1"],
+        "weaponCategory": ["Weapon Category:", "Rifle"],
+        "deadly": ["Deadly:", "Yes"],
+        "favorite": ["Favorite:", "Yes"],
+        "difficulty": ["Difficulty:", "10"],
+        "otherDetails": ["Other Details:", "A semi-automatic .50 caliber rifle."]
+    },
+    "weapon5": {
+        "weaponName": ["Weapon Name:", "Assassin SD"],
+        "weaponCategory": ["Weapon Category:", "Bow"],
+        "deadly": ["Deadly:", "Yes"],
+        "favorite": ["Favorite:", "Yes"],
+        "difficulty": ["Difficulty:", "10"],
+        "otherDetails": ["Other Details:", "A sleek and accurate compound bow."]
+    },
+    "weapon6": {
+        "weaponName": ["Weapon Name:", "Katana"],
+        "weaponCategory": ["Weapon Category:", "Sword"],
+        "deadly": ["Deadly:", "Yes"],
+        "favorite": ["Favorite:", "Yes"],
+        "difficulty": ["Difficulty:", "10"],
+        "otherDetails": ["Other Details:", "A Japanese two-handed sword with a long curve blade with a single-edge."]
+    },
+    "weapon7": {
+        "weaponName": ["Weapon Name:", "Lutel Italian Battle Axe"],
+        "weaponCategory": ["Weapon Category:", "Axe"],
+        "deadly": ["Deadly:", "Yes"],
+        "favorite": ["Favorite:", "No"],
+        "difficulty": ["Difficulty:", "5"],
+        "otherDetails": ["Other Details:", "A one-handed axe with a square-shaped counter weight."]
+    },
+    "weapon8": {
+        "weaponName": ["Weapon Name:", "Tanto"],
+        "weaponCategory": ["Weapon Category:", "Dagger"],
+        "deadly": ["Deadly:", "Yes"],
+        "favorite": ["Favorite:", "No"],
+        "difficulty": ["Difficulty:", "3"],
+        "otherDetails": ["Other Details:", "A Japanese dagger often used by samurai to recover honor through suicide."]
+    },
+    "weapon9": {
+        "weaponName": ["Weapon Name:", "Iberian Mace"],
+        "weaponCategory": ["Weapon Category:", "Mace"],
+        "deadly": ["Deadly:", "Yes"],
+        "favorite": ["Favorite:", "No"],
+        "difficulty": ["Difficulty:", "6"],
+        "otherDetails": ["Other Details:", "Has a wooden haft with steel spikes on one end."]
+    },
+    "weapon10": {
+        "weaponName": ["Weapon Name:", "C4"],
+        "weaponCategory": ["Weapon Category:", "Explosive"],
+        "deadly": ["Deadly:", "Yes"],
+        "favorite": ["Favorite:", "Yes"],
+        "difficulty": ["Difficulty:", "2"],
+        "otherDetails": ["Other Details:", "A kind of plastic explosive."]
+    },
+    "weapon11": {
+        "weaponName": ["Weapon Name:", "Cyanide"],
+        "weaponCategory": ["Weapon Category:", "Poison"],
+        "deadly": ["Deadly:", "Yes"],
+        "favorite": ["Favorite:", "No"],
+        "difficulty": ["Difficulty:", "7"],
+        "otherDetails": ["Other Details:", "It rapid-acting deadly chemical."]
+    },
+    "weapon12": {
+        "weaponName": ["Weapon Name:", "Rubber Chicken"],
+        "weaponCategory": ["Weapon Category:", "Other"],
+        "deadly": ["Deadly:", "No"],
+        "favorite": ["Favorite:", "Yes"],
+        "difficulty": ["Difficulty:", "0"],
+        "otherDetails": ["Other Details:", "Hit people as much as you like, this thing won't do anything."]
+    }
+}
