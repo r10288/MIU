baf12c63447b434740fb85d0e640fe64add1d80e
Ryan Cantone <ryan.cantone@fullsail.edu>
2013-08-01 10:53:51 -0400

    Final
    Final pages upload p4

---
+++ MIU Project 4/Gold App/js/JSON.js
@@ -0,0 +1,273 @@
+/*
+Ryan Cantone
+8-1-13
+Project 4
+1308
+*/
+
+var json = {
+    "weapon1": {
+        "weaponName": ["Weapon Name:", "Beretta Nano"],
+        "weaponCategory": ["Weapon Category:", "Handgun"],
+        "deadly": ["Deadly:", "Almost Always"],
+        "liked": ["Liked:", "Yes"],
+        "difficulty": ["Difficulty:", "4"],
+        "otherDetails": ["Other Details:", "An ultra-compact 9mm pistol."]
+    },
+    "weapon2": {
+        "weaponName": ["Weapon Name:", "Uzi Carbine"],
+        "weaponCategory": ["Weapon Category:", "SMG"],
+        "deadly": ["Deadly:", "Almost Always"],
+        "liked": ["Liked:", "Yes"],
+        "difficulty": ["Difficulty:", "6"],
+        "otherDetails": ["Other Details:", "A legal civilian variant of the popular Uzi."]
+    },
+    "weapon3": {
+        "weaponName": ["Weapon Name:", "Serbu Super Shorty 9"],
+        "weaponCategory": ["Weapon Category:", "Shotgun"],
+        "deadly": ["Deadly:", "Almost Always"],
+        "liked": ["Liked:", "No"],
+        "difficulty": ["Difficulty:", "8"],
+        "otherDetails": ["Other Details:", "A very compact 12 gauge shotgun with a pistol grip."]
+    },
+    "weapon4": {
+        "weaponName": ["Weapon Name:", "Barrett 82A1"],
+        "weaponCategory": ["Weapon Category:", "Rifle"],
+        "deadly": ["Deadly:", "Almost Always"],
+        "liked": ["Liked:", "Yes"],
+        "difficulty": ["Difficulty:", "10"],
+        "otherDetails": ["Other Details:", "A semi-automatic .50 caliber rifle."]
+    },
+    "weapon5": {
+        "weaponName": ["Weapon Name:", "Assassin SD"],
+        "weaponCategory": ["Weapon Category:", "Bow"],
+        "deadly": ["Deadly:", "Almost Always"],
+        "liked": ["Liked:", "Yes"],
+        "difficulty": ["Difficulty:", "10"],
+        "otherDetails": ["Other Details:", "A sleek and accurate compound bow."]
+    },
+    "weapon6": {
+        "weaponName": ["Weapon Name:", "Katana"],
+        "weaponCategory": ["Weapon Category:", "Sword"],
+        "deadly": ["Deadly:", "Almost Always"],
+        "liked": ["Liked:", "Yes"],
+        "difficulty": ["Difficulty:", "10"],
+        "otherDetails": ["Other Details:", "A Japanese two-handed sword with a long curve blade with a single-edge."]
+    },
+    "weapon7": {
+        "weaponName": ["Weapon Name:", "Lutel Italian Battle Axe"],
+        "weaponCategory": ["Weapon Category:", "Axe"],
+        "deadly": ["Deadly:", "Almost Always"],
+        "liked": ["Liked:", "No"],
+        "difficulty": ["Difficulty:", "5"],
+        "otherDetails": ["Other Details:", "A one-handed axe with a square-shaped counter weight."]
+    },
+    "weapon8": {
+        "weaponName": ["Weapon Name:", "Tanto"],
+        "weaponCategory": ["Weapon Category:", "Dagger"],
+        "deadly": ["Deadly:", "Almost Always"],
+        "liked": ["Liked:", "No"],
+        "difficulty": ["Difficulty:", "3"],
+        "otherDetails": ["Other Details:", "A Japanese dagger often used by samurai to recover honor through suicide."]
+    },
+    "weapon9": {
+        "weaponName": ["Weapon Name:", "Iberian Mace"],
+        "weaponCategory": ["Weapon Category:", "Mace"],
+        "deadly": ["Deadly:", "Almost Always"],
+        "liked": ["Liked:", "No"],
+        "difficulty": ["Difficulty:", "6"],
+        "otherDetails": ["Other Details:", "Has a wooden haft with steel spikes on one end."]
+    },
+    "weapon10": {
+        "weaponName": ["Weapon Name:", "C4"],
+        "weaponCategory": ["Weapon Category:", "Compact Explosive"],
+        "deadly": ["Deadly:", "Almost Always"],
+        "liked": ["Liked:", "Yes"],
+        "difficulty": ["Difficulty:", "2"],
+        "otherDetails": ["Other Details:", "A kind of plastic explosive."]
+    },
+    "weapon11": {
+        "weaponName": ["Weapon Name:", "Cyanide"],
+        "weaponCategory": ["Weapon Category:", "Stealthy Weapon"],
+        "deadly": ["Deadly:", "Almost Always"],
+        "liked": ["Liked:", "No"],
+        "difficulty": ["Difficulty:", "7"],
+        "otherDetails": ["Other Details:", "It is rapid-acting deadly chemical."]
+    },
+    "weapon12": {
+        "weaponName": ["Weapon Name:", ".44 Magnum"],
+        "weaponCategory": ["Weapon Category:", "Handgun"],
+        "deadly": ["Deadly:", "Almost Always"],
+        "liked": ["Liked:", "Yes"],
+        "difficulty": ["Difficulty:", "7"],
+        "otherDetails": ["Other Details:", "A handgun with the power of a large rifle."]
+    },
+    "weapon13": {
+        "weaponName": ["Weapon Name:", "P90"],
+        "weaponCategory": ["Weapon Category:", "SMG"],
+        "deadly": ["Deadly:", "Almost Always"],
+        "liked": ["Liked:", "Yes"],
+        "difficulty": ["Difficulty:", "8"],
+        "otherDetails": ["Other Details:", "Sleek gun with clear magazines that you can see while firing."]
+    },
+    "weapon14": {
+        "weaponName": ["Weapon Name:", "12 Gage"],
+        "weaponCategory": ["Weapon Category:", "Shotgun"],
+        "deadly": ["Deadly:", "Almost Always"],
+        "liked": ["Liked:", "No"],
+        "difficulty": ["Difficulty:", "5"],
+        "otherDetails": ["Other Details:", "Short range but devastating if hit."]
+    },
+    "weapon15": {
+        "weaponName": ["Weapon Name:", "M4 A1 Carbine"],
+        "weaponCategory": ["Weapon Category:", "Rifle"],
+        "deadly": ["Deadly:", "Almost Always"],
+        "liked": ["Liked:", "Yes"],
+        "difficulty": ["Difficulty:", "7"],
+        "otherDetails": ["Other Details:", "Standard issue fully automatic assault rifle."]
+    },
+    "weapon16": {
+        "weaponName": ["Weapon Name:", "Practice Bow"],
+        "weaponCategory": ["Weapon Category:", "Bow"],
+        "deadly": ["Deadly:", "Sometimes"],
+        "liked": ["Liked:", "No"],
+        "difficulty": ["Difficulty:", "8"],
+        "otherDetails": ["Other Details:", "A bow that shoots dull arrows for practice purposes."]
+    },
+    "weapon17": {
+        "weaponName": ["Weapon Name:", "Frostmourne"],
+        "weaponCategory": ["Weapon Category:", "Sword"],
+        "deadly": ["Deadly:", "Almost Always"],
+        "liked": ["Liked:", "Yes"],
+        "difficulty": ["Difficulty:", "10"],
+        "otherDetails": ["Other Details:", "Sword used by the Litch King."]
+    },
+    "weapon18": {
+        "weaponName": ["Weapon Name:", "Wood Axe"],
+        "weaponCategory": ["Weapon Category:", "Axe"],
+        "deadly": ["Deadly:", "Rarely"],
+        "liked": ["Liked:", "No"],
+        "difficulty": ["Difficulty:", "3"],
+        "otherDetails": ["Other Details:", "A blunt, thicker bladed axe meant for chopping wood."]
+    },
+    "weapon19": {
+        "weaponName": ["Weapon Name:", "Swiss Army Knife"],
+        "weaponCategory": ["Weapon Category:", "Dagger"],
+        "deadly": ["Deadly:", "Rarely"],
+        "liked": ["Liked:", "Yes"],
+        "difficulty": ["Difficulty:", "1"],
+        "otherDetails": ["Other Details:", "A pocket knife and miniature survival tool."]
+    },
+    "weapon20": {
+        "weaponName": ["Weapon Name:", "Big Stick"],
+        "weaponCategory": ["Weapon Category:", "Mace"],
+        "deadly": ["Deadly:", "Sometimes"],
+        "liked": ["Liked:", "No"],
+        "difficulty": ["Difficulty:", "4"],
+        "otherDetails": ["Other Details:", "A big, blunt stick."]
+    },
+    "weapon21": {
+        "weaponName": ["Weapon Name:", "Claymore"],
+        "weaponCategory": ["Weapon Category:", "Compact Explosive"],
+        "deadly": ["Deadly:", "Almost Always"],
+        "liked": ["Liked:", "Yes"],
+        "difficulty": ["Difficulty:", "6"],
+        "otherDetails": ["Other Details:", "A trap type bomb that explodes when its sensors are triggered."]
+    },
+    "weapon22": {
+        "weaponName": ["Weapon Name:", "Rubber Chicken"],
+        "weaponCategory": ["Weapon Category:", "Improvised Weapon"],
+        "deadly": ["Deadly:", "Rarely"],
+        "liked": ["Liked:", "Yes"],
+        "difficulty": ["Difficulty:", "0"],
+        "otherDetails": ["Other Details:", "Hit people as much as you like, this thing probably won't do anything."]
+    },
+    "weapon23": {
+        "weaponName": ["Weapon Name:", "Water Gun"],
+        "weaponCategory": ["Weapon Category:", "Handgun"],
+        "deadly": ["Deadly:", "Rarely"],
+        "liked": ["Liked:", "Yes"],
+        "difficulty": ["Difficulty:", "0"],
+        "otherDetails": ["Other Details:", "Great for pool centered war games but useless in real combat."]
+    },
+    "weapon24": {
+        "weaponName": ["Weapon Name:", "MP5"],
+        "weaponCategory": ["Weapon Category:", "SMG"],
+        "deadly": ["Deadly:", "Almost Always"],
+        "liked": ["Liked:", "Yes"],
+        "difficulty": ["Difficulty:", "7"],
+        "otherDetails": ["Other Details:", "A light weight SMG often used for stealth missions."]
+    },
+    "weapon25": {
+        "weaponName": ["Weapon Name:", "The Enforcer"],
+        "weaponCategory": ["Weapon Category:", "Shotgun"],
+        "deadly": ["Deadly:", "Almost Always"],
+        "liked": ["Liked:", "Yes"],
+        "difficulty": ["Difficulty:", "10"],
+        "otherDetails": ["Other Details:", "Great gun for keeping in check and your shoulder out of alignment."]
+    },
+    "weapon26": {
+        "weaponName": ["Weapon Name:", "22 Caliber"],
+        "weaponCategory": ["Weapon Category:", "Rifle"],
+        "deadly": ["Deadly:", "Sometimes"],
+        "liked": ["Liked:", "No"],
+        "difficulty": ["Difficulty:", "3"],
+        "otherDetails": ["Other Details:", "A beginner rifle usually used for hunting."]
+    },
+    "weapon27": {
+        "weaponName": ["Weapon Name:", "Horn Bow"],
+        "weaponCategory": ["Weapon Category:", "Bow"],
+        "deadly": ["Deadly:", "Almost Always"],
+        "liked": ["Liked:", "Yes"],
+        "difficulty": ["Difficulty:", "10"],
+        "otherDetails": ["Other Details:", ""]
+    },
+    "weapon28": {
+        "weaponName": ["Weapon Name:", "Frostmourne"],
+        "weaponCategory": ["Weapon Category:", "Sword"],
+        "deadly": ["Deadly:", "Almost Always"],
+        "liked": ["Liked:", "Yes"],
+        "difficulty": ["Difficulty:", "10"],
+        "otherDetails": ["Other Details:", "Sword used by the Litch King."]
+    },
+    "weapon29": {
+        "weaponName": ["Weapon Name:", "Throwing Axe"],
+        "weaponCategory": ["Weapon Category:", "Axe"],
+        "deadly": ["Deadly:", "Almost Always"],
+        "liked": ["Liked:", "No"],
+        "difficulty": ["Difficulty:", "8"],
+        "otherDetails": ["Other Details:", "A small axe balanced to be the perfect weapon to throw at somebody."]
+    },
+    "weapon30": {
+        "weaponName": ["Weapon Name:", "Stiletto"],
+        "weaponCategory": ["Weapon Category:", "Dagger"],
+        "deadly": ["Deadly:", "Sometimes"],
+        "liked": ["Liked:", "no"],
+        "difficulty": ["Difficulty:", "7"],
+        "otherDetails": ["Other Details:", "A dagger with a long, sharp blade."]
+    },
+    "weapon31": {
+        "weaponName": ["Weapon Name:", "Flail"],
+        "weaponCategory": ["Weapon Category:", "Mace"],
+        "deadly": ["Deadly:", "Almost Always"],
+        "liked": ["Liked:", "Yes"],
+        "difficulty": ["Difficulty:", "9"],
+        "otherDetails": ["Other Details:", "A mace similar to a morning star except the ball is on a chain attached to the handle."]
+    },
+    "weapon32": {
+        "weaponName": ["Weapon Name:", "Fireworks"],
+        "weaponCategory": ["Weapon Category:", "Pyrotechnic"],
+        "deadly": ["Deadly:", "Rarely"],
+        "liked": ["Liked:", "Yes"],
+        "difficulty": ["Difficulty:", "3"],
+        "otherDetails": ["Other Details:", "Explosive meant to make beautiful explosions up in the sky."]
+    },
+    "weapon33": {
+        "weaponName": ["Weapon Name:", "Taser"],
+        "weaponCategory": ["Weapon Category:", "Other"],
+        "deadly": ["Deadly:", "Rarely"],
+        "liked": ["Liked:", "Yes"],
+        "difficulty": ["Difficulty:", "5"],
+        "otherDetails": ["Other Details:", "Meant for stunning and subduing an attacker."]
+    }
+};
---
+++ MIU Project 4/Gold App/js/main.js
@@ -0,0 +1,298 @@
+/*
+Ryan Cantone
+1308
+8-1-13
+Project 4
+ */
+
+
+window.addEventListener("DOMContentLoaded", function doItAll(){
+
+    function grab(e){
+        var elementID = document.getElementById(e);
+        return elementID;
+    }
+
+    var /*weaponType = ["--Weapon Category--", "Handgun", "SMG", "Shotgun", "Rifle", "Bow", "Sword", "Axe", "Dagger", "Mace", "Explosive", "Other"]*/
+        deadlyValue,
+        likedValue = "No",
+        saveData = grab("saveData");//variables
+
+    //errMsg = grab("errors");
+
+    /*function makeSelect(){
+     var formTag = document.getElementsByTagName("form"),
+     selectLi = grab("select"),
+     makeSelect = document.createElement("select");
+     makeSelect.setAttribute("id", "weapons");
+     for(var i = 0, j = weaponType.length; i < j; i++){
+     var makeOption = document.createElement("option");
+     var optText = weaponType[i];
+     makeOption.setAttribute("value", optText);
+     makeOption.innerHTML = optText;
+     makeSelect.appendChild(makeOption);
+     }
+     selectLi.appendChild(makeSelect);
+     }*///old error function
+
+    function getRadio(){
+        var radios = document.forms[0].deadly;
+        for(var i = 0; i < radios.length; i++){
+            if (radios[i].checked){
+                var deadlyValue = radios[i].value;
+            }
+        }
+    }//find radio value
+
+    function getCheckbox(){
+        if(grab('liked').checked){
+            var likedValue = grab('liked').value;
+        } else {
+            likedValue = "No";
+        }
+    }//find checkbox value
+
+    function toggleControls(n){
+     switch(n){
+         case "on":
+         grab("addItem").style.display = "none";
+         grab('clearData').style.display = "inline";
+         grab("displayData").style.display = "none";
+         grab("addNew").style.display = "inline";
+         break;
+         case "off":
+         grab("addItem").style.display = "block";
+         grab('clearData').style.display = "inline";
+         grab("displayData").style.display = "inline";
+         grab("addNew").style.display = "none";
+         grab("items").style.display = "none";
+         break;
+         default:
+         return false;
+        }
+     }// controls to toggle links on and off
+
+    /*function validate(e){
+         var getWeaponName = grab("wn");
+         var getWeaponCategory = grab("weapons");
+
+         errMsg.innerHTML = "";
+         getWeaponName.style.border = "1px solid black";
+         getWeaponCategory.style.border = "1px solid black";
+
+         var messageArray = [];
+
+         if(getWeaponName.value === ""){
+             var weaponNameError = "Please enter the weapon's name.";
+             getWeaponName.style.border = "2px solid red";
+             messageArray.push(weaponNameError);
+         }
+
+         if(getWeaponCategory.value === "--Weapon Category--"){
+             var weaponCategoryError = "Please choose a weapon category.";
+             getWeaponCategory.style.border = "2px solid red";
+             messageArray.push(weaponCategoryError);
+         }
+
+         if(messageArray.length >= 1){
+             for(var i = 0, j = messageArray.length; i < j; i++){
+                 var text = document.createElement("li");
+                 text.innerHTML = messageArray[i];
+                 errMsg.appendChild(text);
+             }
+                 e.preventDefault();
+                 return false;
+             } else {
+                 storeData(this.key);
+             }
+     }*///old validate function
+
+    function storeData(key){
+        if(!key){
+            var id = Math.floor(Math.random()*100000001);
+        } else {
+            id = key;
+        }
+        getRadio();
+        getCheckbox();
+        var item = {};
+        item.weaponName = ["Weapon Name:", grab("weaponName").value];
+        item.weaponCategory = ["Weapon Category:", grab("weaponCategory").value];
+        item.deadly = ["Deadly:", deadlyValue];
+        item.liked = ["Liked:", likedValue];
+        item.difficulty = ["Difficulty:", grab("difficulty").value];
+        item.otherDetails = ["Other Details:", grab("otherDetails").value];
+        localStorage.setItem(id, JSON.stringify(item));
+        alert("Weapon Registered!");
+    }//save data to local storage
+
+    function validate(){
+        var parseWeaponForm = function(data){
+            storeData(data);
+        };
+        $(document).ready(function(){
+            var wForm = $("#registerWeaponForm");
+            wForm.validate({
+                invalidHandler: function(form, validator){},
+                submitHandler: function(){
+
+                    var data = wForm.serializeArray();
+                    parseWeaponForm(data);
+                }
+            });
+        });
+    }//validate by making sure that required fields are filled
+
+    function editItem(){
+        var value = localStorage.getItem(this.key);
+        var item = JSON.parse(value);
+
+        toggleControls("off");
+
+        grab("weaponName").value = item.weaponName[1];
+        grab("weaponCategory").value = item.weaponCategory[1];
+        var radios = document.forms[0].deadly;
+        for(var i = 0; i < radios.length; i++){
+            if(radios[i].value == "Almost Always" && item.deadly[1] == "Almost Always"){
+                radios[i].setAttribute("checked", "checked");
+            } else if(radios[i].value == "Sometimes" && item.deadly[1] == "Sometimes"){
+                radios[i].setAttribute("checked", "checked");
+            } else if(radios[i].value == "Rarely" && item.deadly[1] == "Rarely"){
+                radios[i].setAttribute("checked", "checked");
+            }
+        }
+        if(item.liked[1] == "Yes"){
+            grab("liked").setAttribute("checked", "checked");
+        }
+        grab("difficulty").value = item.difficulty[1];
+        grab("otherDetails").value = item.otherDetails[1];
+
+        saveData.removeEventListener("click", storeData);
+
+        grab("saveData").value = "Edit Weapon";
+        var editSubmit = grab("saveData");
+
+        editSubmit.addEventListener("click", validate);
+        editSubmit.key = this.key;
+    }//edit pieces of saved data
+
+    function deleteItem(){
+        var ask = confirm("Are you sure you want to delete this weapon?");
+        if(ask){
+            localStorage.removeItem(this.key);
+            alert("Weapon removed from the Armory.");
+            window.location.reload();
+        } else {
+            alert("Weapon was not removed from the Armory.");
+        }
+    }//delete pieces of saved data
+
+    function autoFillData(){
+        for(var n in json){
+            var id = Math.floor(Math.random()*100000001);
+            localStorage.setItem(id, JSON.stringify(json[n]));
+        }
+    }//fill local storage with test data
+
+    function getImage(catName, makeSubList){
+        var imageLi = document.createElement("li");
+        makeSubList.appendChild(imageLi);
+        var newImage = document.createElement("img");
+        var setSrc = newImage.setAttribute("src", "images/" + catName + ".svg");
+        imageLi.appendChild(newImage);
+
+    }//assign images to saved pieces of data
+
+    function makeItemLinks(key, linksLi){
+        var editLink = document.createElement("a");
+        editLink.href = "#";
+        editLink.key = key;
+        var editText = "Edit Weapon";
+        editLink.addEventListener("click", editItem);
+        editLink.innerHTML = editText;
+        linksLi.appendChild(editLink);
+
+        var breakTag = document.createElement('br');
+        linksLi.appendChild(breakTag);
+
+        var deleteLink = document.createElement("a");
+        deleteLink.href = "#";
+        deleteLink.key = key;
+        var deleteText = "Delete Weapon";
+        deleteLink.addEventListener("click", deleteItem);
+        deleteLink.innerHTML = deleteText;
+        linksLi.appendChild(deleteLink);
+    }//make links inside of local storage
+
+    function getData(){
+        toggleControls("on");
+        if(localStorage.length === 0){
+            var ask = confirm("There are no registered weapons. Would you like to auto-fill the Database?");
+            if(ask){
+                autoFillData();
+            } else {
+                window.location.reload();
+            }
+        }
+        var makeDiv = document.createElement("div");
+        makeDiv.setAttribute("id", "items");
+        makeDiv.setAttribute("data-role", "page");
+
+        var makeList = document.createElement("ul");
+        makeList.setAttribute("data-role","listview");
+        makeList.setAttribute("data-theme", "a");
+        makeList.setAttribute("data-inset","true");
+        makeList.setAttribute("data-filter", "true");
+
+        makeDiv.appendChild(makeList);
+        document.body.appendChild(makeDiv);
+        grab("items").style.display = "block";
+        for(var i = 0, len=localStorage.length; i < len; i++){
+            var makeli = document.createElement("li");
+            makeli.setAttribute("id","listele");
+            var linksLi = document.createElement("div");
+            linksLi.setAttribute("id","divele");
+            makeList.appendChild(makeli);
+            var key = localStorage.key(i);
+            var value = localStorage.getItem(key);
+            var obj = JSON.parse(value);
+            var makeSubList = document.createElement("p");
+            makeli.appendChild(makeSubList);
+            getImage(obj.weaponCategory[1], makeSubList);
+            for(var n in obj){
+                var makeSubli = document.createElement("div");
+                makeSubList.appendChild(makeSubli);
+                var optSubText = obj[n][0] + " " + obj[n][1];
+                makeSubli.innerHTML = optSubText;
+                makeSubList.appendChild(linksLi);
+            }
+            makeItemLinks(localStorage.key(i), linksLi);
+        }
+    }//view data in local storage
+
+    function eraseData(){
+        if(localStorage.length === 0){
+            alert("There are no registered weapons at the moment.");
+        } else {
+            var ask = confirm("Are you sure you want to delete the entire Armory Database?");
+            if(ask){
+                localStorage.clear();
+                alert("The Armory Database has been erased.");
+                window.location.reload();
+                return false;
+            } else {
+                alert("The Armory Database was not deleted.");
+                window.location.reload();
+            }
+        }
+    }//erase everything from local storage
+
+    //makeSelect();
+
+
+    saveData.addEventListener("click", validate);
+    var displayData = grab("displayData");
+    displayData.addEventListener("click", getData);
+    var clearData = grab("clearData");
+    clearData.addEventListener("click", eraseData);
+});
